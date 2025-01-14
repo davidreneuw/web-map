fcae3277902b6ea08adbac865d9c670826d9e428
richard burcher <richard.burcher@nrc.gc.ca>
2025-01-14 14:38:34 -0500

    Add proxy

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, url_for, send_from_directory, render_template
 
 BLOB_DIRECTORY = "/blob"
 
+DEPLOY = True
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,11 +16,21 @@ def index():
 
 @app.route("/image/<filename>")
 def serve_image(filename):
-    return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=False)
+    if DEPLOY:
+        proxy_url = "/app/WEB"
+        full_url = f"{proxy_url}/filename"
+        return send_from_directory(BLOB_DIRECTORY, full_url, as_attachment=False)
+    else:
+        return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=False)
 
 @app.route("/download/<filename>")
 def download_file_from_blob(filename):
-    return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=True)
+    if DEPLOY:
+        proxy_url = "/app/WEB"
+        full_url = f"{proxy_url}/filename"
+        return send_from_directory(BLOB_DIRECTORY, full_url, as_attachment=True)
+    else:
+        return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
