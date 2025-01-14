14efb048dd8abf675074ca12cfd0f80d1c58a46b
richard burcher <richard.burcher@nrc.gc.ca>
2025-01-14 15:55:59 -0500

    Move location of proxy url

--- app.py
+++ app.py
@@ -14,23 +14,22 @@ def index():
     image_filename = "Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi.jpg"
     return render_template("index.html", image_filename=image_filename)
 
-@app.route("/<filename>")
-def serve_file(filename):
-    if DEPLOY:
-        proxy_url = "/app/WEB"
-        full_url = f"{proxy_url}/{filename}"
-        return send_from_directory(BLOB_DIRECTORY, full_url)
-    else:
+if DEPLOY:
+    @app.route("/app/WEB/<filename>")
+    def serve_file(filename):
         return send_from_directory(BLOB_DIRECTORY, filename)
-
-@app.route("/download/<filename>")
-def download_file_from_blob(filename):
-    if DEPLOY:
-        proxy_url = "/app/WEB"
-        full_url = f"{proxy_url}/{filename}"
-        return send_from_directory(BLOB_DIRECTORY, full_url, as_attachment=True)
-    else:
+    
+    @app.route("/app/WEB/download/<filename>")
+    def download_file_from_blob(filename):
         return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=True)
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+else:
+    @app.route("/<filename>")
+    def serve_file(filename):
+        return send_from_directory(BLOB_DIRECTORY, filename)
+
+    @app.route("/download/<filename>")
+    def download_file_from_blob(filename):
+        return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=True)
+
+
