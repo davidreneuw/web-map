4c7de70ec0b104ea6b11cf1f2ad48fffeb2c130a
David René <56747050+davidreneuw@users.noreply.github.com>
2025-01-15 10:49:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,34 @@ import warnings
 from flask import Flask, url_for, send_from_directory, render_template
 
 BLOB_DIRECTORY = "/blob"
+DEPLOY = true
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    image_filename = "Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi.jpg"
-    return render_template("index.html", image_filename=image_filename)
+if DEPLOY:
+    @app.route("/app/WEB/")
+    def index():
+        image_filename = "Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi.jpg"
+        return render_template("index.html", image_filename=image_filename)
 
-@app.route("image/<filename>")
-def serve_file(filename):
-    return send_from_directory(BLOB_DIRECTORY, filename)
+    @app.route("/app/WEB/image/<filename>")
+    def serve_file(filename):
+        return send_from_directory(BLOB_DIRECTORY, filename)
     
-@app.route("download/<filename>")
-def download_file_from_blob(filename):
-    return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=True)
+    @app.route("/app/WEB/download/<filename>")
+    def download_file_from_blob(filename):
+        return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=True)
+else:
+    @app.route("/")
+    def index():
+        image_filename = "Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi.jpg"
+        return render_template("index.html", image_filename=image_filename)
+
+    @app.route("/image/<filename>")
+    def serve_file(filename):
+        return send_from_directory(BLOB_DIRECTORY, filename)
+    
+    @app.route("/download/<filename>")
+    def download_file_from_blob(filename):
+        return send_from_directory(BLOB_DIRECTORY, filename, as_attachment=True)
+    
